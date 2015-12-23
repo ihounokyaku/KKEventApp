672fbb4d866c58ea1a86cc8321dfe3d04f3dc583
zhurbey <air@macbook-air-air.local>
2015-12-23 21:26:16 +0700

    ewtewqt

--- KKEventApp/ViewController.swift
+++ KKEventApp/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     //Does this work??
     // #R#R#
     //32523523523
+    //erherfhdfhtdhetd
 
     override func viewDidLoad() {
         super.viewDidLoad()
